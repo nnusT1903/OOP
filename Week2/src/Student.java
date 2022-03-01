849dfe8bebc76245adbfadbe5cd9479070cdca94
tsunN <21020461@vnu.edu.vn>
2022-03-01 23:55:22 +0700

    01032022

---
+++ Week2/src/Student.java
@@ -0,0 +1,73 @@
+public class Student {
+    //attributes:
+    private String name;
+    private String id;
+    private String email;
+    private String group;
+    /**
+     * Constructor.
+     */
+
+    public Student(){
+        name = "Student";
+        id = "000";
+        group = "K62CB";
+        email = "uet@vnu.edu.vn";
+    }
+
+    /*
+     * Constructor 2.
+     */
+
+    public Student(String name, String id, String email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.group = "K62CB";
+    }
+
+    /**
+     * Getters
+     */
+    public String getName(){
+        return name;
+    }
+
+    public String getGroup(){
+        return group;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    /**
+     * Setter
+     */
+    public void setName(String name){
+        this.name = name;
+    }
+
+    /**
+     * Get information
+     */
+    public String getInfo(){
+        String temp = "";
+        temp = temp + name + " - " ;
+        temp = temp + id + " - " ;
+        temp = temp + group + " - ";
+        temp = temp + email;
+        return temp;
+    }
+
+    /**
+     * Copy Data
+     */
+    public Student(Student s){
+        this.name = s.name;
+        this.id = s.id;
+        this.group = s.group;
+        this.email = s.email;
+    }
+
+}
---
+++ Week2/src/StudentManagement.java
@@ -0,0 +1,75 @@
+import java.util.Objects;
+
+public class StudentManagement {
+    static Student[] students = new Student[100];
+    Integer count = 0;
+    public static boolean sameGroup(Student s1, Student s2){
+        return s1.getGroup().equals(s2.getGroup());
+    }
+
+    public void addStudent(Student newStudent){
+        students[count] = new Student(newStudent);
+        count ++;
+    }
+
+    public String studentsByGroup(){
+        if (students[0] == null) {
+            return "";
+        }
+
+        int n = count;
+        boolean[] used = new boolean[100];
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (used[i]) {
+                continue;
+            }
+            String tmpGroup = students[i].getGroup();
+            ans.append(tmpGroup);
+            ans.append("\n");
+            for (int j = i; j < n; j++) {
+                if (students[j].getGroup().equals(tmpGroup)) {
+                    used[j] = true;
+                    ans.append(students[j].getInfo()).append("\n");
+                }
+            }
+        }
+        return ans.toString();
+    }
+
+    public void removeStudent(String id) {
+        int index = -1;
+        for (int i = 0; i < count; i++) {
+            if (students[i].getId().equals(id)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            System.err.println("Invalid");
+            return;
+        }
+
+        for (int i = index; i < count - 1; i++) {
+            students[i] = students[i + 1];
+        }
+        //reduce count by 1
+        count--;
+    }
+
+    public static void main(String[] args) {
+        StudentManagement solution = new StudentManagement();
+        Student s1 = new Student("Nguyen Van An", "17020001", "17020001@vnu.edu.vn");
+        Student s2 = new Student("Nguyen Van B", "17020002", "17020002@vnu.edu.vn");
+        Student s3 = new Student("Nguyen Van C", "17020003", "17020003@vnu.edu.vn");
+        Student s4 = new Student("Nguyen Van D", "17020004", "17020004@vnu.edu.vn");
+        solution.addStudent(s1);
+        solution.addStudent(s2);
+        solution.addStudent(s3);
+        solution.addStudent(s4);
+        System.out.println(solution.studentsByGroup());
+    }
+}
+
